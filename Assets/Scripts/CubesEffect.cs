49e704f1b150dc36f5eff3eda8b33fcae3a7b726
Nikola Marjanović <nikolals05@outlook.com>
2023-05-25 01:43:08 +0200

    cubesEffect modifications

--- Assets/Scripts/CubesEffect.cs
+++ Assets/Scripts/CubesEffect.cs
@@ -14,28 +14,34 @@ public class CubesEffect : MonoBehaviour
     [SerializeField] private float _spacing;
     [SerializeField] private float _mouseZOffset;
 
+    [Header("Components")]
+    [SerializeField] private Transform _granicnik;
+
     // Private
     private float _startPosX = 0f;
     private float _startPosY = 0f;
     private List<Transform> _cubes = new List<Transform>();
     private List<Vector3> _cubesPositions = new List<Vector3>();
+    private List<Transform> _cubesPositionsPart2 = new List<Transform>();
     private Vector3 _cachedCubeScale;
 
 
     private void Start()
     {
         CreateCubesGrid();
+        CreateCubesGrid2();
         _cachedCubeScale = _cubes[0].localScale;
     }
 
-    float _distanceDeviderMax = 4.5f;
+    float _distanceDeviderMax = 5f; // 4.5f
     float _distanceDeviderMin = 0f;
     float _distanceDevider;
     private void Update()
     {
         //Vector3 mousePos = MouseManager.GetMousePos(_mouseZOffset);
         Vector3 mousePos = Cursor.Position;
-        _distanceDevider = Mathf.Lerp(_distanceDeviderMin, _distanceDeviderMax, ScrollManager.ScrollValue*3 - 2);
+        //_distanceDevider = Mathf.Lerp(_distanceDeviderMin, _distanceDeviderMax, ScrollManager.ScrollValue*3 - 2);
+        _distanceDevider = _distanceDeviderMax;
 
         if (_distanceDevider <= 0)
             return;
@@ -45,9 +51,19 @@ public class CubesEffect : MonoBehaviour
             Transform cube = _cubes[i];
             float distance = Mathf.Clamp01(Vector2.Distance(_cubesPositions[i], mousePos) / _distanceDevider);
 
+            //float d = Vector2.Distance(mousePos, _cubesPositionsPart2[i].position);
+            float d = mousePos.y - _cubesPositionsPart2[i].position.y;
+            if (d <= 4)
+            {
+                cube.position = Vector3.Lerp(cube.position, _cubesPositionsPart2[i].position, 4 * Time.deltaTime);
+                cube.localScale = Vector3.Lerp(cube.localScale, _cachedCubeScale, 4 * Time.deltaTime);
+                continue;
+            }
+
             // Position
             Vector3 dir2 = mousePos - _cubesPositions[i];
-            cube.position = Vector3.Lerp(_cubesPositions[i], _cubesPositions[i] + dir2, distance);
+            Vector3 finalPos = Vector3.Lerp(_cubesPositions[i], _cubesPositions[i] + dir2, distance);
+            cube.position = Vector3.MoveTowards(cube.position, finalPos, Time.deltaTime * 50);
 
             // Rotate
             //cube.LookAt(mousePos);
@@ -55,7 +71,10 @@ public class CubesEffect : MonoBehaviour
             cube.forward = Vector3.Lerp(Vector3.forward, dir, distance);
 
             // Scale
-            cube.localScale = Vector3.Lerp(_cachedCubeScale, new Vector3(6, 6, 6), distance);
+            Vector3 finalScale = Vector3.Lerp(_cachedCubeScale, new Vector3(6, 6, 6), distance);
+            cube.localScale = Vector3.MoveTowards(cube.localScale, finalScale, Time.deltaTime * 250);
+
+
 
 
         }
@@ -73,6 +92,24 @@ public class CubesEffect : MonoBehaviour
                 newCube.SetParent(transform);
                 _cubes.Add(newCube);
                 _cubesPositions.Add(newCube.position);
+            }
+        }
+    }
+
+    // With offset
+    private void CreateCubesGrid2()
+    {
+        float yOffset = _granicnik.position.y * 2;
+        for (int y = 0; y < _rows; y++)
+        {
+            for (int x = 0; x < _columns; x++)
+            {
+                _startPosX = (_columns / -2);
+                _startPosY = -(_rows - 1) / 2f;
+                GameObject newPos = new GameObject();
+                newPos.transform.position = new Vector3(_startPosX + x, _startPosY + y + yOffset, transform.position.z);
+                newPos.transform.SetParent(transform);
+                _cubesPositionsPart2.Add(newPos.transform);
             }
         }
     }
